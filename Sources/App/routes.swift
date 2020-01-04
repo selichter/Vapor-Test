5f7171f2f65e172b88b73224eed6a43433f22c5b
Sarah Lichter <sarahlichter@Sarahs-MacBook-Pro.local>
2020-01-04 17:44:48 -0600

    added route that accepts data from a post and returns json as an InfoResponse

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,10 +16,24 @@ public func routes(_ router: Router) throws {
         let name = try req.parameters.next(String.self)
         return "Hello \(name)"
     }
+    
+//    returns json
+    router.post(InfoData.self, at: "info") { req, data -> InfoResponse in
+        return InfoResponse(request: data)
+        
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
 }
